bfdddf8d99be53cbe1168371c0a5b49e643ff186
YonatanKoifman <yonatanko@campus.technion.ac.il>
2024-04-02 13:12:30 +0300

    fixed same bug in stats

--- pages/stats_page.py
+++ pages/stats_page.py
@@ -191,7 +191,8 @@ def match_team_to_season_name(team_id, teams_df):
         
 def build_all_seasons_df(team_1_name, team_2_name):
     data_dir = os.path.join('Fantasy-Premier-Leaguue', 'data')
-    seasons = os.listdir(data_dir)[3:8]
+    seasons = os.listdir(data_dir)
+    seasons = sorted(seasons, key=lambda x: int(x.split('-')[0]))[3:]
     all_seasons_df = pd.DataFrame()
     for season in seasons:
         data_path = os.path.join(data_dir, season, 'fixtures.csv')
